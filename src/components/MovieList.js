2a09328876d97f752fb28c5d69888f270ba16361
Achraf Sallem <53909364+lachraf@users.noreply.github.com>
2022-03-23 09:41:02 +0100

    Update MovieList.js

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -5,7 +5,7 @@ import MoviesCard from './MoviesCard';
 const MovieList = (props) => {
   return <div className='movieslist'>
 {
-props.movies.map((movies,i)=>(<MoviesCard key={i} item={movies} delete={props.delete} editt={props.editt} handleRate={props.handleRate} rate={props.rate}/>))
+props.movies.map((movies,i)=>(<MoviesCard key={i} item={movies} delete={props.delete} handleRate={props.handleRate} rate={props.rate}/>))
 }
   </div>;
 };
